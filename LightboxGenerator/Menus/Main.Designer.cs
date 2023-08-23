0ea790a623a6a7e3ee4bf3ea5808a79d7b701804
dreamyoe <88789943+dreamyoe@users.noreply.github.com>
2023-08-23 18:48:35 +0200

    Update 1

--- LightboxGenerator/Menus/Main.Designer.cs
+++ LightboxGenerator/Menus/Main.Designer.cs
@@ -32,7 +32,15 @@ namespace LightboxGenerator
             this.quitbo = new System.Windows.Forms.Panel();
             this.quitbox = new System.Windows.Forms.Label();
             this.downloadtemplate = new System.Windows.Forms.Button();
+            this.modname = new System.Windows.Forms.TextBox();
+            this.modauthor = new System.Windows.Forms.TextBox();
+            this.moddescription = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.currenttruck = new System.Windows.Forms.Label();
             this.quitbo.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // quitbo
@@ -74,6 +82,85 @@ namespace LightboxGenerator
             this.downloadtemplate.UseVisualStyleBackColor = false;
             this.downloadtemplate.Click += new System.EventHandler(this.downloadtemplate_Click);
             // 
+            // modname
+            // 
+            this.modname.BackColor = System.Drawing.Color.Silver;
+            this.modname.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.modname.ForeColor = System.Drawing.Color.Black;
+            this.modname.Location = new System.Drawing.Point(155, 125);
+            this.modname.Name = "modname";
+            this.modname.Size = new System.Drawing.Size(200, 13);
+            this.modname.TabIndex = 2;
+            this.modname.Text = "Mod Name";
+            // 
+            // modauthor
+            // 
+            this.modauthor.BackColor = System.Drawing.Color.Silver;
+            this.modauthor.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.modauthor.ForeColor = System.Drawing.Color.Black;
+            this.modauthor.Location = new System.Drawing.Point(155, 144);
+            this.modauthor.Name = "modauthor";
+            this.modauthor.Size = new System.Drawing.Size(200, 13);
+            this.modauthor.TabIndex = 3;
+            this.modauthor.Text = "Mod Author";
+            // 
+            // moddescription
+            // 
+            this.moddescription.BackColor = System.Drawing.Color.Silver;
+            this.moddescription.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.moddescription.ForeColor = System.Drawing.Color.Black;
+            this.moddescription.Location = new System.Drawing.Point(155, 163);
+            this.moddescription.Multiline = true;
+            this.moddescription.Name = "moddescription";
+            this.moddescription.Size = new System.Drawing.Size(200, 144);
+            this.moddescription.TabIndex = 4;
+            this.moddescription.Text = "Mod Description";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(181, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 38);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Mod Info";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(155, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Select Truck";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.currenttruck);
+            this.panel1.Location = new System.Drawing.Point(155, 354);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 31);
+            this.panel1.TabIndex = 8;
+            // 
+            // currenttruck
+            // 
+            this.currenttruck.AutoSize = true;
+            this.currenttruck.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.currenttruck.Location = new System.Drawing.Point(0, 0);
+            this.currenttruck.Name = "currenttruck";
+            this.currenttruck.Size = new System.Drawing.Size(123, 13);
+            this.currenttruck.TabIndex = 0;
+            this.currenttruck.Text = "Current truck mod: None";
+            this.currenttruck.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Window
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -81,6 +168,12 @@ namespace LightboxGenerator
             this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(884, 461);
             this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.moddescription);
+            this.Controls.Add(this.modauthor);
+            this.Controls.Add(this.modname);
             this.Controls.Add(this.downloadtemplate);
             this.Controls.Add(this.quitbo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -90,7 +183,10 @@ namespace LightboxGenerator
             this.Text = "Euro Truck Simulator 2 Lightbox maker";
             this.quitbo.ResumeLayout(false);
             this.quitbo.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -99,6 +195,13 @@ namespace LightboxGenerator
         private System.Windows.Forms.Panel quitbo;
         private System.Windows.Forms.Label quitbox;
         private System.Windows.Forms.Button downloadtemplate;
+        public System.Windows.Forms.TextBox modauthor;
+        public System.Windows.Forms.TextBox modname;
+        public System.Windows.Forms.TextBox moddescription;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label currenttruck;
     }
 }
 
--- LightboxGenerator/Menus/Main.cs
+++ LightboxGenerator/Menus/Main.cs
@@ -8,11 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using LightboxGenerator.Core;
+using LightboxGenerator.Menus;
 
 namespace LightboxGenerator
 {
     public partial class Window : Form
     {
+        private SelectTruck selectTruck;
+
         public Window()
         {
             InitializeComponent();
@@ -26,6 +29,13 @@ namespace LightboxGenerator
         private void downloadtemplate_Click(object sender, EventArgs e)
         {
             Downloader.ShowDownloadPresetWindow();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            selectTruck = new SelectTruck();
+
+            selectTruck.ShowDialog();
         }
     }
 }
